da9e7380d5f64b0b6c8c3a2228bab82fafb88b3a
Yoann Dufour <yoann01.d@gmail.com>
2018-10-05 17:12:56 +0200

    delete unused

--- src/services/socket-client.js
+++
@@ -1,9 +0,0 @@
-class SocketClient extends WebSocket {
-    /**
-     * @param {string} name
-     * @param {Object} data
-     */
-    sendToServer(name, data) {
-        this.send(JSON.stringify({ name, data }));
-    }
-}
