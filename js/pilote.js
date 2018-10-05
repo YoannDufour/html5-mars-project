b753446387814af9cd393df000555a052a4ea3b6
JorisMillot <38904722+JorisMillot@users.noreply.github.com>
2018-10-05 14:27:52 +0200

    fix

--- js/pilote.js
+++ js/pilote.js
@@ -5,6 +5,33 @@ var audio = new Audio();
 audio.src = "sound/motor.mp3";
 audio.volume = 0.2;
 
+var ws, modal;
+
+function selectRole(){
+    document.getElementById("formConnection").action = document.getElementById("jobSelect").value+'.html';
+}
+
+function loadFile(event) {
+    var output = document.getElementById('output');
+    var avatar = URL.createObjectURL(event.target.files[0]);
+    output.src = avatar;
+};
+
+window.onload = function () {
+// Get the modal
+    modal = document.getElementById('modalConnection');
+
+// Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+    modal.style.display = "block";
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+};
+
 
 function Connection() {
     ws = new WebSocket('ws://92.222.88.16:9090' +
